86266e51289afa425f6a5882c5d4cb59c84c92df
ahmed <ahmed.salama1679@gmail.com>
2020-11-17 21:09:48 +0200

    [ADD] new module moataber_purchase_changes/

---
+++ moataber_purchase_changes/models/stock_picking_changes.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+from odoo.exceptions import Warning
+# Ahmed Salama Code Start ---->
+
+
+class StockPickingInherit(models.Model):
+	_inherit = 'stock.picking'
+	
+	driver_name = fields.Char("Driver Name")
+	car_number = fields.Char("Car Number")
+	port_weight = fields.Char("Port Balance Weight")
+
+# Ahmed Salama Code End.
